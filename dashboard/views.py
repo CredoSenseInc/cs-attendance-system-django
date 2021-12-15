32dbd174b75be1f19531d92965c291748e1e74ee
Mehadi Hassan <mehadihassan116@gmail.com>
2021-12-15 22:33:51 +0600

    create_daily_log method added

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from attendance.models import *
+from settings.models import *
 from datetime import date
 import datetime
 
@@ -10,10 +11,72 @@ def dashboard(request):
     todays_date = date.today()
     log_list = attendanceLog.objects.filter(date__month=todays_date.month)
     daily_log_list = attendanceLog.objects.filter(date=todays_date)
+    total_emp = employee.objects.all().count()
+    present_count = 10
+    late_count = 10
+    absent_count = 10
     context = {
         "log_list" : log_list,
         "daily_log_list" : daily_log_list,
         "current_month" : (todays_date).strftime("%B"),
         "current_date" : (todays_date).strftime("%d-%b-%Y"),
+        "total_emp" : total_emp,
+        "present_count" : present_count,
+        "late_count" : late_count,
+        "absent_count" : absent_count,
     }
+    create_daily_log(request)
     return render(request, 'dashboard/dashboard.html', context)
+
+@login_required(login_url='user/login/')
+def create_daily_log(request):
+    todays_date = date.today()
+    all_emp = employee.objects.all()
+    settings = settings_db.objects.last()
+    offDay = str(settings.offDay).split(",")
+    workDay = str(settings.workDay).split(",")
+
+    for i in range (0, len(offDay)):
+        # print(offDay[i])
+        offDay[i] = datetime.datetime.strptime(offDay[i], '%m/%d/%Y').date()
+        # print(offDay[i])
+
+    for i in range (0, len(workDay)):
+        # print(offDay[i])
+        workDay[i] = datetime.datetime.strptime(workDay[i], '%m/%d/%Y').date()
+        # print(offDay[i])
+    weekends = []
+
+    if settings.week_saturday:
+        weekends.append("Saturday")
+    if settings.week_sunday:
+        weekends.append("Sunday")
+    if settings.week_monday:
+        weekends.append("Monday")
+    if settings.week_tuesday:
+        weekends.append("Tuesday")
+    if settings.week_wednesday:
+        weekends.append("Wednesday")
+    if settings.week_thursday:
+        weekends.append("Thursday")
+    if settings.week_friday:
+        weekends.append("Friday")
+
+    print(todays_date.strftime('%A'))
+    print(weekends)
+
+    if (todays_date.strftime('%A') in weekends or todays_date in offDay and todays_date not in workDay):
+        print("Today is weekend")
+    
+    elif(todays_date.strftime('%A') not in weekends or todays_date not in offDay or todays_date  in workDay):
+        print("Today is workday")
+        all_emp = employee.objects.all()
+        check_today_log = attendanceLog.objects.filter(date = todays_date).count()
+        print(check_today_log)
+        if (check_today_log == 0):
+            for i in range (len(all_emp)):
+                attendance = attendanceLog()
+                attendance.emp = all_emp[i]
+                attendance.finger = all_emp[i]
+                attendance.date = todays_date
+                attendance.save()
